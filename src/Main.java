d90ae845042ce68fe227d885bb7fbff953e3934f
kasko <gumapukk@gmail.com>
2023-05-09 18:14:00 +0500

    pas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void  main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+
+        CheckLogin.cheсker("Popp77e_007");
+        CheckPassword.cheсker("oveeee346");
+        CheckConfirmPassword.cheсker("petrov001");
+
+
     }
 }
